e67a0f5e66b4553eeea916f79c825461235028fe
Helen Chong <119173961+helenclx@users.noreply.github.com>
2023-07-14 14:26:08 +0800

    Removed deployment files

--- 1-digital-business-card/vite.config.js
+++ 1-digital-business-card/vite.config.js
@@ -2,15 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/Scrimba-React-Solo-Projects/1-digital-business-card/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
 })
