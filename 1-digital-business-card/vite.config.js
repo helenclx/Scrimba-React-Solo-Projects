309b1df631b62d0c960643aa80d452af628e2279
Helen Chong <119173961+helenclx@users.noreply.github.com>
2023-07-14 13:39:00 +0800

    Deploy business card to GIthub

--- 1-digital-business-card/vite.config.js
+++ 1-digital-business-card/vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Scrimba-React-Solo-Projects/1-digital-business-card/',
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/Scrimba-React-Solo-Projects/1-digital-business-card/'
+  }
+
+  return config
 })
